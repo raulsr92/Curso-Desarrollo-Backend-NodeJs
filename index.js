56d4e5a7d6a1c503d99e8b97bd1761d147f64b37
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-24 21:12:58 -0500

    Importancion de paquete express y elaboracion de 1er endpoint GET + inicializacion de servidor

---
+++ index.js
@@ -0,0 +1,60 @@
+console.log("Hola Mundo, soy Raúl")
+
+// Importación de paquetes
+
+import express from 'express'
+const app = express();
+
+
+// Arreglo de eventos
+
+const arrEventoJson={
+    "arreglo": [
+        {
+            "IdEvento" : 3,
+            "Fecha" : "2025-10-12",
+            "Horario" : "22:30",
+            "IdLocal" : 7,
+            "IdArtista" : 8,
+            "IdCategoria" : 2,
+        },
+        {
+            "IdEvento" : 4,
+            "Fecha" : "2025-05-06",
+            "Horario" : "20:30",
+            "IdLocal" : 10,
+            "IdArtista" : 10,
+            "IdCategoria" : 2,
+        },
+        {
+            "IdEvento" : 5,
+            "Fecha" : "2025-10-06",
+            "Horario" : "21:30",
+            "IdLocal" : 10,
+            "IdArtista" : 9,
+            "IdCategoria" : 2,
+        },
+        {
+            "IdEvento" : 7,
+            "Fecha" : "2025-10-10",
+            "Horario" : "20:30",
+            "IdLocal" : 8,
+            "IdArtista" : 8,
+            "IdCategoria" : 2,
+        }
+    ]}
+
+
+// GET Request
+
+app.get( "/eventos", (req, res) =>{
+
+    res.send(arrEventoJson.arreglo)
+
+} );
+
+// Inicializar el servidor
+
+app.listen("4001", ()=>{
+    console.log("Listening on "+ 4001)
+})
