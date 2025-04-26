ca8c3157c49cbd851f88d63bdbd6f5afe005d1c9
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-26 18:23:34 -0500

    Modularizando los servicios mediante capas (router): creacion de controller de eventos

--- index.js
+++ index.js
@@ -10,45 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 
-// Arreglo de eventos
-
-const arrEventoJson={
-    "arreglo": [
-        {
-            "IdEvento" : 3,
-            "Fecha" : "2025-10-12",
-            "Horario" : "22:30",
-            "IdLocal" : 7,
-            "IdArtista" : 8,
-            "IdCategoria" : 2,
-        },
-        {
-            "IdEvento" : 4,
-            "Fecha" : "2025-05-06",
-            "Horario" : "20:30",
-            "IdLocal" : 10,
-            "IdArtista" : 10,
-            "IdCategoria" : 2,
-        },
-        {
-            "IdEvento" : 5,
-            "Fecha" : "2025-10-06",
-            "Horario" : "21:30",
-            "IdLocal" : 10,
-            "IdArtista" : 9,
-            "IdCategoria" : 2,
-        },
-        {
-            "IdEvento" : 7,
-            "Fecha" : "2025-10-10",
-            "Horario" : "20:30",
-            "IdLocal" : 8,
-            "IdArtista" : 8,
-            "IdCategoria" : 2,
-        }
-    ]}
-
-
+/*
 // GET Request
 
 app.get( "/eventos", (req, res) =>{
@@ -65,6 +27,7 @@ app.get( "/eventos/:id", function(req, res){
     res.send(arrEventoJson.arreglo[req.params.id])
 })
 
+
 // POST Request
 
 app.post("/eventos", function (req,res) {
@@ -80,6 +43,8 @@ app.post("/eventos", function (req,res) {
     arrEventoJson.arreglo.push(objEvento)
 
 })
+
+*/
 
 
 // Inicializar el servidor
