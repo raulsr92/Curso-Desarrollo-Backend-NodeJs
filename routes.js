a4b93ec255e0b04c0d9afdcd3f4a9b3f40b98c35
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-26 22:30:28 -0500

    Modularización: Creando el adminsitrador de rutas

--- routes.js
+++ routes.js
@@ -1,1 +1,18 @@
- 
+// Importación de paquetes
+
+import express from 'express' 
+
+import reventos from './routes/eventos.routes.js'
+
+// Instancias
+
+const router = express.Router()
+
+// Redireccion de rutas
+
+router.use("/eventos",reventos);
+
+
+// Exportar el router
+
+export default router;
