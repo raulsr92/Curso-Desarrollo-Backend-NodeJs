67125f283f9139e79a0de1ddbabb660009a67f2d
Raul Sanchez R. <raul.sr92@gmail.com>
2025-05-05 17:11:00 -0500

    Autenticacion con token: Creacion de controller login

--- controllers/seguridad.controller.js
+++ controllers/seguridad.controller.js
@@ -1,5 +1,6 @@
 
 import * as sseguridad from '../services/seguridad.service.js'
+import * as auth       from '../config/auth.js'
 
 // Funciones exportables
 
@@ -24,3 +25,55 @@ export const logueo = function(req, res){
         res.status(500).json({"error":"Error logueandose"});
     }
 )}
+
+// ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método Login
+
+export const login = function (req, res) {
+
+    console.log("------------controller------------");
+
+    const reqUsuario = req.body;
+    console.log(reqUsuario)
+    
+    sseguridad.login(reqUsuario)
+    .then((usuarios) => {
+        //Validar que exista usuario
+        if (usuarios[0]) {
+
+            //Validar que la contraseña seal a misma a la almacenada en BD
+
+            if (usuarios[0].pass_usuario == reqUsuario.pass_usuario) {
+
+                let token = auth.generateToken(usuarios[0])
+                let refreshToken = auth.generateRefreshToken(usuarios[0])
+
+                console.log(`Token: ${token}`)
+                console.log(`RefreshToken: ${refreshToken}`)
+
+                // Respuesta al cliente
+                res.json(
+                    {
+                        token,
+                        refreshToken,
+                        "user":{
+                            "id_persona": usuarios[0].id_persona,
+                            "correo_usuario": usuarios[0].correo_usuario,
+                            "rol_usuario": usuarios[0].rol_usuario,
+                        }
+                    }
+                )
+            } else {
+                res.status(403).json({"error":"Acceso no autorizado"})
+            }
+
+        } else{
+            res.status(403).json({"error":"Acceso no autorizado"})
+        }
+      }
+    )
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({"error":"Error obteniendo registros"});
+    });
+}
+
