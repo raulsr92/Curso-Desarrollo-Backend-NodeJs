1a04a3c206e710b249f57d675ddecaf57adb773a
Raul Sanchez R. <raul.sr92@gmail.com>
2025-05-05 15:06:44 -0500

    Autenticacion con token: Creacion de los microservicios de seguridad: login y findUserById

--- services/seguridad.service.js
+++ services/seguridad.service.js
@@ -23,3 +23,61 @@ export const logueo = function (objLogin) {
     }
     )
 }
+
+// ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método Login
+
+export const login = function (objUsuario) {
+    console.log("----------------------Servicio de Login--------------------")
+    return new Promise ((resolve, reject)=>{
+        pool.query( 
+            `
+                select 
+	                id_usuario,nom_usuario,correo_usuario,pass_usuario,rol_usuario,
+	                case 
+		                when Activo = 1 then 'Usuario Activo'
+		                when Activo = 0 then 'Usuario Inactivo'
+	                end as Activo
+                from
+                tb_usuario
+                where correo_usuario=? and fingreso_usuario=1
+            `
+            ,[objUsuario.correo_usuario]
+            ,(err, results,fields)=>
+            {
+                console.log(results);
+                if(err){
+                    reject(err)
+                } else{
+                    resolve(results)
+                }
+            })
+    })
+}
+
+// ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método Fin user by ID
+
+export const findUserById = function (id_usuario) {
+    console.log("----------------------Servicio de Login--------------------")
+    return new Promise ((resolve, reject)=>{
+        pool.query( 
+            `
+                select 
+	                id_usuario,nom_usuario,correo_usuario,pass_usuario,rol_usuario,
+	                case 
+		                when Activo = 1 then 'Usuario Activo'
+		                when Activo = 0 then 'Usuario Inactivo'
+	                end as Activo
+                from tb_usuario  where id_usuario=? and fingreso_usuario=1
+            `
+            ,[id_usuario]
+            ,(err, results,fields)=>
+            {
+                console.log(results);
+                if(err){
+                    reject(err)
+                } else{
+                    resolve(results)
+                }
+            })
+    })
+}
